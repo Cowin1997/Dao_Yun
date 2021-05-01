4ab16f2b3d6826dec8252c94ba9c5a379e06ed65
lixibin <1134956893@qq.com>
2021-05-01 17:53:44 +0800

    no message

---
+++ src/test/java/cn/edu/fzu/daoyun/daoyun/DaoYunApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.fzu.daoyun.daoyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoYunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
